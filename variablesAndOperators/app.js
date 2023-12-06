a511d2a481f9daa9134b56919ab04c6b7607b572
Ana Mihajlovska <anamihajlovskaa123@gmail.com>
2023-12-06 18:13:46 +0100

    Homework 01

---
+++ variablesAndOperators/app.js
@@ -0,0 +1,17 @@
+console.log ("====Exercise1===")
+let π = prompt ("Enter the value of π");
+let r2 = prompt ("Enter the value of r2");
+
+let parsedπ = parseInt (π);
+let parsedr2 = parseInt (r2);
+
+let area = parsedπ * parsedr2;
+console.log(area);
+
+console.log ("===Exercise2===");
+let NumberOfPhones = 30;
+let priceOfOne = 119.95;
+let tax = 0.05;
+
+let sum = (119.95 + (119.95 * 0.05)) * 30;
+console.log(sum);
